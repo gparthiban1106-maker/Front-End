c11dabbdf954213e9e52c0fa25ec74f3d7f935ec
gparthiban1106-bhar <gparthiban1106@gmail.com>
2026-06-05 08:16:28 +0530

    new

---
+++ Javascript/learn-slice.js
@@ -0,0 +1,26 @@
+//Example - 1
+let arr=[10,20,30,40,50];
+let res=arr.slice(1,5);
+console.log("Eg1 - ",res);
+console.log("Eg1 - ",arr);
+
+//Example - 2
+let arr1=[10,20,30,40,50];
+let copy=arr.slice();
+console.log("Eg2 - ",copy);
+console.log("Eg2 - ",arr1);
+
+
+//Example - 3
+let arr2=[10,20,30,40,50];
+console.log("Eg3 - ",arr2.slice((2)));
+
+
+//Example - 4
+let arr4=[10,20,30,40,50];
+console.log("Eg4 - ",arr4.slice((-2)));
+
+
+//Example - 5
+let str="JAVACRIPT";
+console.log("Eg5 - ",str.slice(0,4));
---
+++ Javascript/learn-templates-literal.js
@@ -0,0 +1,8 @@
+let name = "Parthiban";
+let num = "12345ABC12345";
+
+let message = `You Have Successfully Created Your Account
+Account Name is ${name}
+Account Number is ${num}`;
+
+console.log(message);
