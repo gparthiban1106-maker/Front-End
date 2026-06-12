690dc8117f9b6208afefb4456a5a9d5636530018
gparthiban1106-bhar <gparthiban1106@gmail.com>
2026-06-12 18:16:37 +0530

    new-3

---
+++ Javascript/learn-destructring.js
@@ -0,0 +1,39 @@
+//destructring - extracting the data from the collected value;
+
+/*const colors =["red","Green","Blue"];
+const first=colors[0];
+const second=colors[1];
+const third=colors[2];
+
+console.log(first);
+console.log(second);
+console.log(third);
+*/
+
+//  with destructring
+/*
+const colorsD=["red","Green","Blue"];
+
+const[first,second,third]=colorsD;
+
+console.log(first);
+console.log(second);
+console.log(third);
+*/
+
+//object with
+
+/*const emplyeeD=
+{
+    name:"David",
+    role:"Code",
+    company:"ABC",
+};
+const {name,role,company}=emplyeeD;
+console.log(name);
+console.log(role);
+console.log(company);*/
+
+
+
+ 
